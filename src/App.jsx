94e7cdec57f7cd06c69b3aca7e175c47d6381fa7
mohkeytea <mohkeytea@gmail.com>
2024-08-30 19:06:33 +0300

    updated1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import ExplorePage from './Pages/ExplorePage/ExplorePage';
 import Profile from './Components/Profile/Profile';
 import StoryWriting from './Components/StoryWriting/StoryWriting';
 import Monetization from './Components/Monetization/Monetization';
-import DraftListPage from './Pages/DraftListPage/DraftListPage';
 import HomePage from './Pages/HomePage/HomePage';
 
 const App = () => {
@@ -24,7 +23,6 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/write" element={<StoryWriting />} />
-          <Route path="/drafts" element={<DraftListPage />} />
         </Routes>
     </Router>
   );
